941db7d256b0b3b9c9dea9d9521ebf5965e2592e
tianyunzheng <tianyunzheng@localhost>
2023-10-26 11:20:47 -0500

    ok

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -14,7 +14,15 @@ public class RecursiveMethods {
 	public static double geometricSum(int n) {
 		
 			// FIXME compute the geometric sum for the first n terms recursively
+		if (n == 0) {
+			
 			return 0;
+			
+		} else {
+			
+			return 1/Math.pow(2, n) + geometricSum(n-1);
+		}		
+		
 		
 	}
 
@@ -29,7 +37,15 @@ public class RecursiveMethods {
 	public static int gcd(int p, int q) {
 		
 			// FIXME compute the gcd of p and q using recursion
-			return 0;
+		if (p % q == 0) {
+			
+			return q;
+			
+		} else {
+			
+			return gcd(q, p % q);
+		}
+		
 		
 	}
 
@@ -42,9 +58,22 @@ public class RecursiveMethods {
 	 * @return an array with the same data as the input but it reverse order
 	 */
 	public static int[] toReversed(int[] array) {
-		
+		int[] reversed = new int[array.length];
 			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
+			int n = array.length - 1;
+			for (int i = 0; i < n; i++) {
+				reversed[i] = array[i];
+			}
+		if (array.length == 0) {
+			return reversed;
+			
+		} else {
+			return helper (array, 0, reversed);
+		}
+	}
+	public static int[] helper(int[]array, int index, int[]reversed) {
+		for ()
+		return helper (array, 1, reversed);
 		
 	}
 
